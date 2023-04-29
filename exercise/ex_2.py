03de9a1840da1dbf356e06cc7b3a2acb0e026dd9
Wiktoria Nitka <wiktoriamnitka.gmail.com>
2023-04-29 22:27:59 +0200

    "Prostokąt"

--- exercise/ex_2.py
+++ exercise/ex_2.py
@@ -1,3 +1,5 @@
+
+
 a = 10
 b = 20
 c = 15
@@ -6,4 +8,13 @@ h = 12
 obwod = a + b + c
 pole = int((h * a) / 2)
 
-print("Obwod trojkata wynosi " + str(obwod) + ", zas pole wynisi " + str(pole) + ".")
+print("Obwod trojkata wynosi " + str(obwod) +
+      ", zas pole wynisi " + str(pole) + ".")
+
+a = 10
+b = 20
+
+obwod_prostokat = a+b+a+b
+pole_prostokat = a*b
+print("Obwod prostokata wynosi " + str(obwod_prostokat) +
+      ", zas pole wynisi " + str(pole_prostokat) + ".")
