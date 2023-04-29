3c5e015a3671a804a3645901f5958c32069f6770
Wiktoria Nitka <wiktoriamnitka.gmail.com>
2023-04-29 22:38:49 +0200

    Koło

--- exercise/ex_2.py
+++ exercise/ex_2.py
@@ -1,5 +1,6 @@
+import math
 
-
+#Trojkat
 a = 10
 b = 20
 c = 15
@@ -10,7 +11,7 @@ pole = int((h * a) / 2)
 
 print("Obwod trojkata wynosi " + str(obwod) +
       ", zas pole wynisi " + str(pole) + ".")
-
+#Prostokat
 a = 10
 b = 20
 
@@ -18,3 +19,14 @@ obwod_prostokat = a+b+a+b
 pole_prostokat = a*b
 print("Obwod prostokata wynosi " + str(obwod_prostokat) +
       ", zas pole wynisi " + str(pole_prostokat) + ".")
+
+#Kolo
+
+r = 10
+pi = math.pi
+
+obwod_kolo = 2*pi*r
+pole_kolo = pi*r**2
+
+print("Obwod kola wynosi " + str(obwod_kolo) +
+      ", zas pole wynisi " + str(pole_kolo) + ".")
