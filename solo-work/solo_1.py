d67cac1f812a46067a2f991fe622d4335fd79fe5
Wiktoria Nitka <wiktoriamnitka.gmail.com>
2023-04-30 09:36:35 +0200

    Added zadanie 1.1

---
+++ solo-work/solo_1.py
@@ -0,0 +1,11 @@
+# zadanie 1.1
+
+# zadanie 1.1
+
+hello = "Hello"
+student = "Ola"
+
+# oczekiwany rezultat: Hello Ola
+# wykorzystaj w princie zmienne hello i student
+print (f'{hello} {student}')
+
