4caa26fc75bb9ae68b1a512232da73594b41c2c1
Wiktoria Nitka <wiktoriamnitka.gmail.com>
2023-05-01 13:04:17 +0200

    Added zadanie 1.2

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -1,11 +1,15 @@
 # zadanie 1.1
 
-# zadanie 1.1
-
 hello = "Hello"
 student = "Ola"
 
 # oczekiwany rezultat: Hello Ola
 # wykorzystaj w princie zmienne hello i student
 print (f'{hello} {student}')
+
+# zadanie 1.2
+
+student = input("Enter your name:")
+print(f'{hello} {student}')
+
 
